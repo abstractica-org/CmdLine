c3ff29ed822baf61d377069a4c89074253268bf9
tgrundtvig <TGrundtvig@gmail.com>
2022-11-02 17:44:20 +0100

    Minor updates

--- src/main/java/org/abstractica/cmdline/CmdLine.java
+++ src/main/java/org/abstractica/cmdline/CmdLine.java
@@ -1,5 +1,9 @@
 package org.abstractica.cmdline;
 
+import org.abstractica.base.io.textio.TextInput;
+import org.abstractica.base.io.textio.TextOutput;
+import org.abstractica.base.io.textio.terminal.TerminalOutput;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -7,7 +11,7 @@ import java.util.function.Consumer;
 
 public class CmdLine
 {
-	public static boolean runCommand(String cmd)
+	public static void runCommand(String cmd, TextOutput processOut)
 	{
 		try
 		{
@@ -15,30 +19,17 @@ public class CmdLine
 					.toLowerCase().startsWith("windows");
 			String prefix = isWindows ? "cmd.exe /c " : "sh -c ";
 			Process process = Runtime.getRuntime().exec(prefix + cmd);
-			StreamReader streamReader =
-					new StreamReader(process.getInputStream(), System.out::println);
-			streamReader.run();
+			new BufferedReader(new InputStreamReader(process.getInputStream())).lines()
+					.forEach(str ->processOut.println(str));
 			int exitCode = process.waitFor();
-			return exitCode == 0;
 		}catch(Exception e)
 		{
-			return false;
+			throw new RuntimeException("Could not run command: " + cmd, e);
 		}
 	}
 
-	private static class StreamReader implements Runnable {
-		private InputStream inputStream;
-		private Consumer<String> consumer;
-
-		public StreamReader(InputStream inputStream, Consumer<String> consumer) {
-			this.inputStream = inputStream;
-			this.consumer = consumer;
-		}
-
-		@Override
-		public void run() {
-			new BufferedReader(new InputStreamReader(inputStream)).lines()
-					.forEach(consumer);
-		}
+	public static void runCommand(String cmd)
+	{
+		runCommand(cmd, new TerminalOutput());
 	}
 }
